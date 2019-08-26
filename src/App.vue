5b3b23d57851e0a1fd368a6e842a0e8af1172b63
Songsungeun <korea.sesong@gmail.com>
2019-08-26 18:25:28 +0900

    작업 중간 커밋 (Refresh 아이콘 추가)

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,20 @@
 
     <v-content>
       <map-area class="map_body"></map-area>
+      <v-btn
+        text
+        small
+        @click="refresh"
+      > 
+        <v-img
+          :src="require('./assets/refresh.svg')"
+          class="my-3"
+          contain
+          height="20"
+          width="20"
+          id="refresh_btn"
+        ></v-img>
+      </v-btn>
     </v-content>
   </v-app>
 </template>
